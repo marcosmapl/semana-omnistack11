d818a601c234db50ee6cda91ba188fbbb9c28d27
Marcos Lima <marcos.lima@icomp.ufam.edu.br>
2020-03-28 16:07:53 -0400

    feat(security): add cors to express application

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,10 @@
-const express =  require('express');
+const express = require('express');
+const cors = require('cors');
 const routes = require('./routes');
 
 const app = express();
 
+app.use(cors());
 app.use(express.json()); // define que as requisições devam ocorrer usando JSON
 
 /**
