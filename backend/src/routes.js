06f28122067e10b62a0a4768e0e48b462a43910f
Marcos Lima <marcos.lima@icomp.ufam.edu.br>
2020-03-28 14:57:48 -0400

    feat(controllers): add an incidents controllers for database operations

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,10 +1,14 @@
 const express =  require('express');
 const OngController = require('./controllers/OngController');
+const IncidentController = require('./controllers/IncidentController');
 
 // obtém o objeto de roteamento do express
 const routes = express.Router();
 
 routes.get('/ongs', OngController.index);
 routes.post('/ongs', OngController.create);
+
+routes.get('/incidents', IncidentController.index);
+routes.post('/incidents', IncidentController.create);
 
 module.exports = routes;
