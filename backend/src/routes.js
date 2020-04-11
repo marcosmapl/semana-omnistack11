7e7ef75ae00409450a9287407e6193983156f19c
Marcos Lima <marcos.lima@icomp.ufam.edu.br>
2020-04-11 17:56:48 -0400

    docs(routes): add code docs describing routes paths

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -11,13 +11,18 @@ const routes = express.Router();
 
 // ONG ROUTES
 
+// rota responsável por validar o acesso da 'ong' ao sistema (logon).
+// o 'id' da 'ong' deve ser enviado no corpo da requisição
 routes.post('/sessions', celebrate({
   [Segments.BODY]: Joi.object().keys({
     id: Joi.string().required(),
   }),
 }), SessionController.index);
 
+// rota de listagem de 'ongs'. 
 routes.get('/ongs', OngController.index);
+
+// rota de inclusão de 'ongs' (cadastro)
 routes.post('/ongs', celebrate({
   [Segments.BODY]: Joi.object().keys({
     name: Joi.string().required(),
@@ -28,12 +33,14 @@ routes.post('/ongs', celebrate({
   })
 }), OngController.create);
 
+// rota de listagem dos 'incidentes' da 'ong'
 routes.get('/profile', celebrate({
   [Segments.HEADERS]: Joi.object({
     authorization: Joi.string().required(),
   }).unknown(),
 }), ProfileController.index);
 
+// rota de exclusão de uma 'ong' e seus 'incidentes'
 routes.delete('/ongs/:id', celebrate({
   [Segments.HEADERS]: Joi.object({
     authorization: Joi.string().required(),
@@ -45,6 +52,9 @@ routes.delete('/ongs/:id', celebrate({
 
 // INCIDENTS ROUTES
 
+// rota de 'inclusão' de um 'incidente' de uma 'ong'
+// o 'id' da 'ong' deve ser passado como 'authorization' no cabeçalho da requisição
+// os dados do 'incidente' devem vir no corpo da requisição
 routes.post('/incidents', celebrate({
   [Segments.HEADERS]: Joi.object({
     authorization: Joi.string().required(),
@@ -56,13 +66,21 @@ routes.post('/incidents', celebrate({
   }),
 }), IncidentController.create);
 
+// rota de listagem dos 'incidentes'
+// o parametro 'page' é utilizado apenas para paginação (opcional)
 routes.get('/incidents', celebrate({
   [Segments.QUERY]: Joi.object().keys({
     page: Joi.number(),
   })
 }), IncidentController.index);
 
+// rota de exclusão de 'incidente'
+// o 'id' do 'incidente' deve ser passado no parametro 'id' da url
+// o 'id' da 'ong' da registrada é enviado no cabeçalho da requisição e deve ser o mesmo que o cadastrado no banco de dados.
 routes.delete('/incidents/:id', celebrate({
+  [Segments.HEADERS]: Joi.object({
+    authorization: Joi.string().required(),
+  }).unknown(),
   [Segments.PARAMS]: Joi.object().keys({
     id: Joi.number().required(),
   })
