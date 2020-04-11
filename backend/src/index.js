4770c4b8bd43fec85280172b01f4ecb6edc4a8aa
Marcos Lima <marcos.lima@icomp.ufam.edu.br>
2020-04-11 11:31:50 -0400

    feat(routes): add validation to 'ongs' create route, using 'celebrate' and 'joi'

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { errors } = require('celebrate');
 const routes = require('./routes');
 
 const app = express();
@@ -34,5 +35,6 @@ app.use(express.json()); // define que as requisições devam ocorrer usando JSO
  */
 
 app.use(routes);
+app.use(errors());
 
 app.listen(3333);
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,4 +1,6 @@
-const express =  require('express');
+const express = require('express');
+const { celebrate, Segments, Joi } = require('celebrate');
+
 const OngController = require('./controllers/OngController');
 const IncidentController = require('./controllers/IncidentController');
 const ProfileController = require('./controllers/ProfileController');
@@ -10,7 +12,15 @@ const routes = express.Router();
 routes.post('/sessions', SessionController.index);
 
 routes.get('/ongs', OngController.index);
-routes.post('/ongs', OngController.create);
+routes.post('/ongs', celebrate({
+  [Segments.BODY]: Joi.object().keys({
+    name: Joi.string().required(),
+    email: Joi.string().required().email(),
+    whatsapp: Joi.string().required().min(13).max(14),
+    city: Joi.string().required(),
+    uf: Joi.string().required().length(2),
+  })
+}), OngController.create);
 
 routes.get('/incidents', IncidentController.index);
 routes.post('/incidents', IncidentController.create);
